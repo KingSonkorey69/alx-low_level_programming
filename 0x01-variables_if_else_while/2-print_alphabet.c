d1321b151946cb48b5d459f93f41207b6623ec96
KingSonkorey69 <sam.kimotho450@gmail.com>
2023-03-17 04:48:57 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print Lowercase Alphabet(a-z).
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'abcdefghijklmnopqrstuvwxyz';
+	putchar("Lowercase English Alphabets:\n", ch);
+	return (0);
+}
