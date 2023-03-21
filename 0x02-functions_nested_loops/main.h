2847bee146036da2d1624f00f73578fced59434c
KingSonkorey69 <sam.kimotho450@gmail.com>
2023-03-21 08:29:28 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,13 @@
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
+#include "main.h"
+/**
+ *_isalpha - a function that checks for alphabetic character.
+ * @c: char type letter
+ * Return: 1 if lowercase, 0 if not lowercase.
+ */
+int _isalpha(int c);
+{
+if (c > 96 && c < 122 || c > 65 && c < 90)
+return (1);
+else
+return (0);
+}
