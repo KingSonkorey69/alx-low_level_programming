34a1716c84870ac590e2719de7d9380629d5e930
KingSonkorey69 <sam.kimotho450@gmail.com>
2023-03-21 09:05:21 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,9 @@
  * @c: type int character
  * Return: 1 if letter, lowercase or uppercase, and 0 otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
-if ((c > 65 && c < 90) || (c > 96 && c < 122))
+if ((c > 64 && c < 91) || (c > 96 && c < 123))
 return (1);
 else
 return (0);
