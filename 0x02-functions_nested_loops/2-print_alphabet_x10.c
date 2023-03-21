9c5172ae0b06b5dad56de66d3e52e596f66a42b4
KingSonkorey69 <sam.kimotho450@gmail.com>
2023-03-21 06:49:44 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,18 @@
  */
 void print_alphabet_x10(void)
 {
-	int i = 0;
-	char ch;
+int i = 0;
+char ch;
 
-	while (i < 10)
-	{
-		ch = 'a';
-		while (ch <= 'z')
-		{
-			_putchar(ch);
-			c++;
-		}
-		_putchar('\n');
-		i++;
-	}
+while (i < 10)
+{
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+i++;
+}
 }
