58aa587c19c6f586db94f58040279fe8b92ea028
KingSonkorey69 <sam.kimotho450@gmail.com>
2023-03-21 11:19:29 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_to_98 - Print all natural numbers from n to 98
+ * @n: int type number
+ */
+
+void print_to_98(int n)
+{
+int x;
+for (x < 0; x <= 200; x++)
+{
+_putchar("%d", x);
+}
+_putchar("\n");
+
+}
