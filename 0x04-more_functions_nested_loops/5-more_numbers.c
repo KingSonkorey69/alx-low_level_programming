d6f778fc8d3db7957ca150604c846400a41517d5
KingSonkorey69 <sam.kimotho450@gmail.com>
2023-03-23 05:47:07 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,26 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
-* more_numbers - Print 10 times the numbers since 0 up to 14
-*
-* Return: 10 times of the numbers since 0 up to 14
-*/
+ * more_numbers - print 10 times the numbers from 0 to 14
+ * Description: Can only use _putchar 3 times
+ */
 void more_numbers(void)
 {
-int x, y;
+	int c, i;
 
-for (x = 0; x < 10; x++)
-{
-for (y = 0; y <= 14; y++)
-{
-if (y > 9)
-{
-_putchar((y / 10) + '0');
-}
+	c = 0;
+	while (c < 10)
+	{
+		i = 0;
+		while (i < 15)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
 
-_putchar((y % 10) + '0');
-}
-
-_putchar('\n');
-}
+			_putchar(i % 10 + '0');
+			i++;
+		}
+		c++;
+		_putchar('\n');
+	}
 }
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,20 @@
 #include "main.h"
+
 /**
-* print_line - Draws a straight line according to parameter
-* @n: The number of lines to draw
-*
-* Return: empty
-*/
+ * print_line - draw a straight line
+ * @n: number of times underscore is printed
+ * Description: Can only use _putchar to print
+ */
 void print_line(int n)
 {
-int x;
+	int c;
 
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
-for (x = 0; x < n; x++)
-{
-_putchar(95);
-}
+	c = 0;
+	while (c < n)
+	{
+		_putchar('_');
 
-_putchar('\n');
-}
+		c++;
+	}
+	_putchar('\n');
 }
