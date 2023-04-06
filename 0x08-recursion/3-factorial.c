93c049335037750f038c09ef8b73f1464f97dbc5
KingSonkorey69 <sam.kimotho450@gmail.com>
2023-04-06 03:11:40 -0700

    101-wildcmp.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,23 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * factorial - return factorial of a given number.
- * @n: int type number
- * Return: factorial of number
+ * factorial - Returns the factorial of a given number.
+ * @n: The number to find the factorial of.
+ *
+ * Return: If n > 0 - the factorial of n.
+ *         If n < 0 - 1 to indicate an error.
  */
-
 int factorial(int n)
 {
+	int result = n;
+
 	if (n < 0)
 		return (-1);
-	else if (n < 2)
-		return (n);
-	else
-		return (n * factorial(n - 1));
+
+	else if (n >= 0 && n <= 1)
+		return (1);
+
+	result *= factorial(n - 1);
+
+	return (result);
 }
